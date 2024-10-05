aed91d6972baa21e03c740f68facfef85bc91abe
yigtuyumz <yigtuyumz@gmail.com>
2024-10-05 03:36:03 +0300

    Max Karl Ernst Ludwig Planck

--- i3scripts/i3bar/playinfo.sh
+++ i3scripts/i3bar/playinfo.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
 
+#
+# NOT COMPLETED YET !!!
+#
+
 song_title="$(playerctl metadata xesam:title)"
 artist="$(playerctl metadata xesam:artist)"
 album="$(playerctl metadata xesam:album)"
 
 echo "${artist} | ${song_title} - ${album}"
-
 
--- i3scripts/nitrogen/setbg.sh
+++ i3scripts/nitrogen/setbg.sh
@@ -1,15 +1,21 @@
 #!/usr/bin/env bash
 
-# what if values of --head changes??
+# what if value of --head changes?? (1 becomes 0 or viceversa)
 
-bg_path='/usr/share/backgrounds'
+bgfile="${HOME}/.local/share/black.png"
+
+
+if ! [ -f "${bgfile}" ]; then
+  ffmpeg -f lavfi -i color=c=black:s=2x2:d=1 -frames:v 1 "${bgfile}" \
+    2>/dev/null
+fi
+
 monitor_count=$(xrandr | grep '\bconnected\b' | cut -d' ' -f1 | wc -w)
 
 # set bg for main display, always
-nitrogen --head=1 --set-zoom-fill "${bg_path}/black.png" 2>/dev/null
+nitrogen --head=1 --set-zoom-fill "${bgfile}" 2>/dev/null
 
 # set bg for auxilary display, if connected
 if [[ "${monitor_count}" -ge 2 ]]; then
-	nitrogen --head=0 --set-zoom-fill "${bg_path}/black.png" 2>/dev/null
+	nitrogen --head=0 --set-zoom-fill "${bgfile}" 2>/dev/null
 fi
-
